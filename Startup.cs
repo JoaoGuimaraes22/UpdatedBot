136147172e347d0a7c9b00af6eca9f7df057af0e
Joao Guimaraes <Joao.Guimaraes@ctr.unipartner.com>
2019-11-26 11:09:12 +0000

    Setting up LUIS keys and enpoints

--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.Extensions.Hosting;
 using NewBot.Bots;
 using NewBot.Dialogs;
 
@@ -20,7 +20,11 @@ namespace NewBot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //Sets ASP.NET Core compatibility version
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            //Sets MvcOptions.EnableEnpointRouting to false
+            services.AddMvc(options => options.EnableEndpointRouting = false);
 
             // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
@@ -48,7 +52,7 @@ namespace NewBot
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
